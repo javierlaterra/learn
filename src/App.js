d350fb7e26926d9e588926bbaf2c675da9e34cf7
javierlaterra <laterrajavier@gmail.com>
2022-01-01 20:01:44 -0300

    new changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        Micho pregunta
+        <nav className="nav">navegar... y se marcho .... </nav>
       </header>
+      <div className="padre">
+        <section className="sectionl">
+          section
+          <header className="headerl"> heder</header>
+          <article className="articuloi">
+            articulo
+            <header className="header2">header</header>
+            <p className="parrafo">parrafo</p>
+            <div className="papa">
+              <p className="parrafo1">parrafo</p>
+              <aside className="aside1">aside</aside>
+            </div>
+            <footer className="pie">footer</footer>
+          </article>
+        </section>
+
+        <section className="sectionr">
+          section
+          <header className="headerr">extra extra</header>
+          <nav className="navr"> y nave.. otra vez</nav>
+        </section>
+      </div>
+      <footer className="piepie">footer</footer>
     </div>
   );
 }
-
-export default App;
