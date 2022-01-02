340a2189724ffcd2f23200f6804839fb1211328d
javierlaterra <laterrajavier@gmail.com>
2022-01-02 14:44:51 -0300

    calculadora

--- src/App.js
+++ src/App.js
@@ -3,6 +3,48 @@ import "./App.css";
 
 export default function App() {
   return (
-    < 
+    <div className="App">
+      
+    <div className="result"></div>
     
-    />
+     <div className="padre">
+     
+      <div className="fila1" >  <booton className= "c">c</booton> 
+        <booton className= "dividido">/</booton> 
+      </div>
+      <div className="fila2" > <booton className= "siete">7</booton>
+        <booton className= "ocho">8</booton>
+          <booton className= "nueve">9</booton>
+            <booton className= "por">x</booton>
+      </div>
+      <div className="fila3" > <booton className= "cuatro">4</booton>
+        <booton className= "cinco">5</booton>
+          <booton className= "seis">6</booton>
+            <booton className= "menos">-</booton> 
+      </div>
+      <div className="fila4" ><booton className= "uno">1</booton>
+        <booton className= "dos">2</booton>
+          <booton className= "tres">3</booton>
+            <booton className= "mas">+</booton> 
+
+      </div>
+      <div className="fila5" > <booton className= "cero">0</booton>
+          <booton className= "igual">=</booton>
+      </div>
+     
+     
+     
+     
+     
+     </div>
+     
+     
+     
+     
+     
+     
+      </div>
+      
+      );
+      }
+    
