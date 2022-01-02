bb498b545a8ffe951c8cd5248b1c81b0c9a2f8f4
javierlaterra <laterrajavier@gmail.com>
2022-01-02 20:36:27 -0300

    1.0

--- src/App.js
+++ src/App.js
@@ -9,36 +9,36 @@ export default function App() {
   return (
     <div className="App">
       
-    <input type= "number" className="result"></input>
+    <input type= "text" className="result"></input>
     
      <div className="padre">
      
       <div className="fila1" > 
        <button className="c" id="c" onClick={buttonHandler}>c</button> 
-       <button className="dividido">/</button> 
+       <button className="dividido" id="/" onClick={buttonHandler}>/</button> 
       </div>
       <div className="fila2" >
-        <button className= "siete">7</button>
-        <button className= "ocho">8</button>
-        <button className= "nueve">9</button>
-        <button className= "por">x</button>
+        <button className= "siete" id="7" onClick={buttonHandler} >7</button>
+        <button className= "ocho" id="8" onClick={buttonHandler}>8</button>
+        <button className= "nueve" id="9" onClick={buttonHandler}>9</button>
+        <button className= "por" id="x" onClick={buttonHandler}>x</button>
       </div>
       <div className="fila3" >
-        <button className= "cuatro">4</button>
-        <button className= "cinco">5</button>
-        <button className= "seis">6</button>
-        <button className= "menos">-</button> 
+        <button className= "cuatro" id="4" onClick={buttonHandler}>4</button>
+        <button className= "cinco" id="5" onClick={buttonHandler}>5</button>
+        <button className= "seis" id="6" onClick={buttonHandler}>6</button>
+        <button className= "menos" id="-" onClick={buttonHandler}>-</button> 
       </div>
       <div className="fila4" >
-        <button className= "uno">1</button>
-        <button className= "dos">2</button>
-        <button className= "tres">3</button>
-        <button className= "mas">+</button> 
+        <button className= "uno" id="1" onClick={buttonHandler}>1</button>
+        <button className= "dos" id="2" onClick={buttonHandler} >2</button>
+        <button className= "tres" id="3" onClick={buttonHandler}>3</button>
+        <button className= "mas" id="+" onClick={buttonHandler} >+</button> 
 
       </div>
       <div className="fila5" > 
-      <button className= "cero">0</button>
-      <button className= "igual">=</button>
+      <button className= "cero" id="0" onClick={buttonHandler}>0</button>
+      <button className= "igual" id="=" onClick={buttonHandler}>=</button>
       </div>
      
      
