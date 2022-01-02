b7ac562d788744057ba8ccf1e1ba847183714915
javierlaterra <laterrajavier@gmail.com>
2022-01-02 19:42:03 -0300

    cal

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import "./App.css";
 
 
 export default function App() {
+ const buttonHandler = (event) => {
+   
+  console.log(event.target.id)
+  }
   return (
     <div className="App">
       
@@ -9,27 +13,32 @@ export default function App() {
     
      <div className="padre">
      
-      <div className="fila1" >  <button className= "c">c</button> 
-        <button className= "dividido">/</button> 
+      <div className="fila1" > 
+       <button className="c" id="c" onClick={buttonHandler}>c</button> 
+       <button className="dividido">/</button> 
       </div>
-      <div className="fila2" > <button className= "siete">7</button>
+      <div className="fila2" >
+        <button className= "siete">7</button>
         <button className= "ocho">8</button>
-          <button className= "nueve">9</button>
-            <button className= "por">x</button>
+        <button className= "nueve">9</button>
+        <button className= "por">x</button>
       </div>
-      <div className="fila3" > <button className= "cuatro">4</button>
+      <div className="fila3" >
+        <button className= "cuatro">4</button>
         <button className= "cinco">5</button>
-          <button className= "seis">6</button>
-            <button className= "menos">-</button> 
+        <button className= "seis">6</button>
+        <button className= "menos">-</button> 
       </div>
-      <div className="fila4" ><button className= "uno">1</button>
+      <div className="fila4" >
+        <button className= "uno">1</button>
         <button className= "dos">2</button>
-          <button className= "tres">3</button>
-            <button className= "mas">+</button> 
+        <button className= "tres">3</button>
+        <button className= "mas">+</button> 
 
       </div>
-      <div className="fila5" > <button className= "cero">0</button>
-          <button className= "igual">=</button>
+      <div className="fila5" > 
+      <button className= "cero">0</button>
+      <button className= "igual">=</button>
       </div>
      
      
