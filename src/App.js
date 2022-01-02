935611ae46782396752e44c05add5771c19903cb
javierlaterra <laterrajavier@gmail.com>
2022-01-02 16:15:51 -0300

    correccion button input

--- src/App.js
+++ src/App.js
@@ -5,31 +5,31 @@ export default function App() {
   return (
     <div className="App">
       
-    <div className="result"></div>
+    <input type= "number" className="result"></input>
     
      <div className="padre">
      
-      <div className="fila1" >  <booton className= "c">c</booton> 
-        <booton className= "dividido">/</booton> 
+      <div className="fila1" >  <button className= "c">c</button> 
+        <button className= "dividido">/</button> 
       </div>
-      <div className="fila2" > <booton className= "siete">7</booton>
-        <booton className= "ocho">8</booton>
-          <booton className= "nueve">9</booton>
-            <booton className= "por">x</booton>
+      <div className="fila2" > <button className= "siete">7</button>
+        <button className= "ocho">8</button>
+          <button className= "nueve">9</button>
+            <button className= "por">x</button>
       </div>
-      <div className="fila3" > <booton className= "cuatro">4</booton>
-        <booton className= "cinco">5</booton>
-          <booton className= "seis">6</booton>
-            <booton className= "menos">-</booton> 
+      <div className="fila3" > <button className= "cuatro">4</button>
+        <button className= "cinco">5</button>
+          <button className= "seis">6</button>
+            <button className= "menos">-</button> 
       </div>
-      <div className="fila4" ><booton className= "uno">1</booton>
-        <booton className= "dos">2</booton>
-          <booton className= "tres">3</booton>
-            <booton className= "mas">+</booton> 
+      <div className="fila4" ><button className= "uno">1</button>
+        <button className= "dos">2</button>
+          <button className= "tres">3</button>
+            <button className= "mas">+</button> 
 
       </div>
-      <div className="fila5" > <booton className= "cero">0</booton>
-          <booton className= "igual">=</booton>
+      <div className="fila5" > <button className= "cero">0</button>
+          <button className= "igual">=</button>
       </div>
      
      
