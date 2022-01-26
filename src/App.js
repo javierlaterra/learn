9577976ddc936456e6b93a6cc2f54cbf0ba24762
javierlaterra <laterrajavier@gmail.com>
2022-01-26 15:35:59 -0300

    explicacion stado boton y texto

--- src/App.js
+++ src/App.js
@@ -12,12 +12,20 @@ function App() {
     setTexto("");
   }
 
+  const getTexto = () => {
+    // haces algo aca
+    return texto
+  }
+
   return (
     <div className="App">
       <input className= "uno" value={texto} onChange={textChange}/>
       <button onClick={handleClick}>Borrar</button>
+      {/* estos dos de abajo son lo mismo */}
+      <p>Texto para mostrar</p>
     </div>
   );
 }
 
 export default App;
+// hacer un nuevo componente que tenga un boton y un <p> que sea un buleano pero que diga verdadero o falso con un if
